d7c5281eabc00a43bb69b25cce4e9676cee4337b
KanegaeGabriel <gabrielksouza2009@hotmail.com>
2020-02-01 05:04:26 -0300

    Add placement to names_ranking.txt

--- names_generate_ranking.py
+++ names_generate_ranking.py
@@ -17,6 +17,9 @@ class Aluno:
         self.curso = curso
         self.nota = float(self.nota) / (1 + float(self.bonus)/100) # Remove any bonus they might have
 
+    def __str__(self):
+        return "{:>6.2f} - {} - {} (#{})".format(self.nota, self.nome, self.curso, self.posicao)
+
 t0 = time()
 
 directory = "data"
@@ -50,8 +53,8 @@ alunos = sorted(alunos, key=lambda x: (x.nota), reverse=True)
 
 # Write to .txt
 with open(os.path.join(directory, filename + "_ranking" + ".txt"), "w+", encoding="UTF-8") as humanFile:
-    for i, a in enumerate(alunos):
+    for i, aluno in enumerate(alunos):
         pct = (1-(i+1)/len(alunos))*100
-        humanFile.write("{:>6}: {:>7.3f}% - {:>6.2f} - {} - {}\n".format(i+1, pct, a.nota, a.nome, a.curso))
+        humanFile.write("{:>6}: {:>7.3f}% - {}\n".format(i+1, pct, aluno))
 
 print("Written {} students to '{}_ranking.txt' in {:.1f}s.".format(len(alunos), directory+"/"+filename, time()-t0))
